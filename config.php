2daa00e56fdb06918672ac38a55af408e6927561
Henry Wrightman <wrigh928@umn.edu>
2018-04-03 21:08:27 -0500

    config anon

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// MySQL keys
+	define('mysql_host', 'localhost');
+	define('mysql_user', '');
+	define('mysql_password', '');
+	define('mysql_db', '');
+	define('domain', 'http://elastic-swimmer.000webhostapp.com');
+
+	// Shortened links config
+	define('short_len', 5);
+	define('short_characters', '0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ'); 
+?>
